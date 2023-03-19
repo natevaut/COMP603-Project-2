df3773265ca6edff5747594218441f97294caf3b
nate <jmy3092@autuni.ac.nz>
2023-03-19 18:25:35 +1300

    start pet care functionality with feeding

--- src/assignment/Program.java
+++ src/assignment/Program.java
@@ -1,5 +1,7 @@
 package assignment;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -34,7 +36,7 @@ public class Program {
         do {
             System.out.print("Please select a pet to look after: ");
 
-            char petChar = scanner.next().toUpperCase().charAt(0);
+            char petChar = scanner.nextLine().toUpperCase().charAt(0);
 
             switch (petChar) {
                 case 'D':
@@ -53,21 +55,64 @@ public class Program {
                     System.out.println("That is not a valid pet option.");
             }
         } while (animal == null);
-        
+
         // Name the animal
-        System.out.println("Name your animal: ");
+        System.out.print("Name your animal: ");
         String petName = scanner.nextLine();
+        animal.setName(petName);
         System.out.println();
 
         // Print chosen pet
         System.out.println("Chosen pet:");
         System.out.println(animal.toString().toUpperCase());
         System.out.println();
-        System.out.println(animal.getName().toUpperCase());
         ASCII.printAnimal(animal);
+        System.out.println(petName.toUpperCase());
         System.out.println();
-        
 
+        // Keep the animal alive
+        HashMap<String, Float> hydration = animal.getHydrationData();
+        while (true) {
+            int need = rand.nextInt(4);
+            System.out.print(petName + " is ");
+            switch (need) {
+                case 1: {
+                    System.out.println(asPercent(1 - animal.getNutrition()) + "% hungry!");
+
+                    String[] foods = animal.getFoods();
+                    HashMap<String, Float> nutrition = animal.getNutritionData();
+
+                    String food = null;
+                    while (food == null) {
+                        System.out.println("What will you feed " + petName + "?");
+                        System.out.printf("%s, %s, or %s\n", foods[0], foods[1], foods[2]);
+
+                        String inputFood = scanner.next().toLowerCase();
+                        if (nutrition.containsKey(inputFood)) {
+                            food = inputFood;
+                        } else {
+                            System.out.println("That is not a valid food!");
+                        }
+                        animal.eat(food);
+                        System.out.println("Replenished " + petName + "'s hunger to " + asPercent(animal.getNutrition()));
+                    }
+                    break;
+                }
+                case 2: {
+                    System.out.println("thirsty!");
+                    break;
+                }
+                case 3: {
+                    System.out.println("lonely :(");
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private String asPercent(float amount) {
+        return String.format("%.1f%%", amount * 100);
     }
 
 }
