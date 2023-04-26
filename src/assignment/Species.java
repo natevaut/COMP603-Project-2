407f8c098265de5ee59ebcaab8d93efb6e2ef90a
nate <jmy3092@autuni.ac.nz>
2023-04-26 13:31:24 +1200

    make it easier to add a new species
    no longer have to change the main method to add a new species

--- src/assignment/Species.java
+++ src/assignment/Species.java
@@ -3,4 +3,24 @@ package assignment;
 /** @author Nate Evans 21144881 */
 public enum Species {
     DOG, CAT, RABBIT, HAMSTER, FROG;
+
+    public static String[] getSpeciesList() {
+        return new String[] { "Dog", "Cat", "Rabbit", "Hamster", "Frog" };
+    }
+    
+    public static Animal newSpeciesFromString(String species) {
+        switch (species) {
+            case "Dog":
+                return new Dog();
+            case "Cat":
+                return new Cat();
+            case "Rabbit":
+                return new Rabbit();
+            case "Hamster":
+                return new Hamster();
+            case "Frog":
+                return new Frog();
+        }
+        return null;
+    }
 }
