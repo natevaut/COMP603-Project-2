905e8b1fcdb19f378333d56a17b306b2226b11d6
vina <alvina.angelin.s@gmail.com>
2023-06-08 20:02:45 +1200

    finish test

--- src/test/animals/TestHamster.java
+++ src/test/animals/TestHamster.java
@@ -1,5 +1,29 @@
 package test.animals;
 
-public class TestHamster {
+import org.junit.jupiter.api.Test;
 
+import animals.Hamster;
+
+/** @author Alvina Angelin 22152692 */
+
+class TestHamster {
+	@Test
+	void testHamster() {
+		assert new Hamster().getSpecies().name().equalsIgnoreCase("hamster");
+	}
+
+	@Test
+	void testHamsterStats() {
+		Hamster hamster = new Hamster();
+		hamster.drink("water");
+		assert hamster.getHydration() == 0.7f;
+		hamster.drink("juice");
+		assert hamster.getHydration() == 0.8f;
+		hamster.eat("seeds");
+		assert hamster.getNutrition() == 0.1f;
+		hamster.receive("pat");
+		assert hamster.getLove() == 0.4f;
+		hamster.receive("hug");
+		assert hamster.getLove() == 0.6f;
+	}
 }
--- src/test/animals/TestRabbit.java
+++ src/test/animals/TestRabbit.java
@@ -1,5 +1,29 @@
 package test.animals;
 
-public class TestRabbit {
+import org.junit.jupiter.api.Test;
 
+import animals.Rabbit;
+
+/** @author Alvina Angelin 22152692 */
+
+class TestRabbit {
+	@Test
+	void testRabbit() {
+		assert new Rabbit().getSpecies().name().equalsIgnoreCase("rabbit");
+	}
+
+	@Test
+	void testRabbitStats() {
+		Rabbit rabbit = new Rabbit();
+		rabbit.drink("water");
+		assert rabbit.getHydration() == 0.7f;
+		rabbit.drink("juice");
+		assert rabbit.getHydration() == 0.8f;
+		rabbit.eat("carrot");
+		assert rabbit.getNutrition() == 0.6f;
+		rabbit.receive("pat");
+		assert rabbit.getLove() == 0.4f;
+		rabbit.receive("cuddle");
+		assert rabbit.getLove() == 0.7f;
+	}
 }
