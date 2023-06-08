c08d6ba21a0d42034105220c372c447737698a0e
nate <jmy3092@autuni.ac.nz>
2023-06-08 18:48:18 +1200

    fix FileIO test

--- src/test/fileio/TestFileIO.java
+++ src/test/fileio/TestFileIO.java
@@ -2,6 +2,7 @@ package test.fileio;
 
 import org.junit.jupiter.api.Test;
 
+import animals.Animal;
 import animals.Cat;
 import animals.Dog;
 import animals.Species;
@@ -11,19 +12,22 @@ public class TestFileIO {
 
 	@Test
 	void testSaveToFile() {
-		Dog dog = new Dog();
+		Animal dog = new Dog();
 		dog.setName("DAVE");
 		assert FileIO.saveToFile(dog);
-		Cat cat = new Cat();
+		Animal cat = new Cat();
 		cat.setName("STEVE");
 		assert FileIO.saveToFile(cat);
-		assert FileIO.saveToFile(Species.newSpeciesFromString("rabbit"));
+		Animal rabbit = Species.newSpeciesFromString("rabbit");
+		rabbit.setName("HARRY");
+		assert FileIO.saveToFile(rabbit);
 	}
 
 	@Test
 	void testLoadFromFile() {
 		assert FileIO.loadFromFile().containsKey("DAVE");
 		assert FileIO.loadFromFile().containsKey("STEVE");
+		assert FileIO.loadFromFile().containsKey("HARRY");
 	}
 
 }
