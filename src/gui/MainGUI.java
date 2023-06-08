d1a6c7f44f898ceaef826063cb539611e5b3d6d4
nate <jmy3092@autuni.ac.nz>
2023-06-08 22:39:36 +1200

    Change button UI skin

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 /**
  * @author Alvina Angelin 22152692
@@ -8,6 +9,9 @@ import javax.swing.SwingUtilities;
 public class MainGUI {
 
     public static void main(String[] args) {
+    	// set button look and feel
+        UIManager.put("ButtonUI", "javax.swing.plaf.basic.BasicButtonUI");
+
         SwingUtilities.invokeLater(() -> {
             new PetGUI();
         });
